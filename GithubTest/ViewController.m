884bf33e6091f52ec1c47308f6517509f3fef8a4
Sha Zhu <Cinn@Shas-MacBook-Pro.local>
2015-03-10 14:17:57 -0700

    Initial Commit

---
+++ GithubTest/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  GithubTest
+//
+//  Created by Sha Zhu on 3/10/15.
+//  Copyright (c) 2015 Cena Entertaiment. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ GithubTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GithubTest
+//
+//  Created by Sha Zhu on 3/10/15.
+//  Copyright (c) 2015 Cena Entertaiment. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
